d259ddcadade8f855971b93499a3c38914ab7d4a
nceba <ncebamandizvidza@gmail.com>
2022-09-22 12:43:53 -0700

    Fixture for Task 6 and Task 7

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,7 +7,7 @@
 */
 char *cap_string(char *str)
 {
-	int index = 0
+	int index = 0;
 
 	while (str[++index])
 	{
@@ -15,18 +15,18 @@ char *cap_string(char *str)
 			index++;
 
 		if (str[index - 1] == ' ' ||
-				str[index - 1 == '\t' ||
-				str[index - 1 == '\n' ||
-				str[index - 1 == ',' ||
-				str[index - 1 == ';' ||
-				str[index - 1 == '.' ||
-				str[index - 1 == '!' ||
-				str[index - 1 == '?' ||
-				str[index - 1 == '"' ||
-				str[index - 1 == '(' ||
-				str[index - 1 == ')' ||
-				str[index - 1 == '{' ||
-				str[index - 1 == '}' ||
+				str[index - 1] == '\t' ||
+				str[index - 1] == '\n' ||
+				str[index - 1] == ',' ||
+				str[index - 1] == ';' ||
+				str[index - 1] == '.' ||
+				str[index - 1] == '!' ||
+				str[index - 1] == '?' ||
+				str[index - 1] == '"' ||
+				str[index - 1] == '(' ||
+				str[index - 1] == ')' ||
+				str[index - 1] == '{' ||
+				str[index - 1] == '}')
 			str[index] -= 32;
 	}
 	return (str);
