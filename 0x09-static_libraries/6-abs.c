d968fe35c4cbeaef911569e5bce6b327197b7b21
nceba <ncebamandizvidza@gmail.com>
2022-09-30 18:11:39 -0700

    static libraries

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - pirnts the absolute function of the numebr
+ * @n: number to be evaluated
+ * Return: absolute value of the number
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	else
+		return (n);
+}
